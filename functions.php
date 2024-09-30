8388efb5b68f7315ff0e5215c85b6e417c0b7452
Muhammad-Haram <haramh643@gmail.com>
2024-09-30 12:47:53 -0700

    attached css and js file

---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php
+
+function load_css()
+{
+	wp_register_style('swiper', get_template_directory_uri() . '/css/swiper.min.css', array(), false, 'all');
+	wp_enqueue_style('swiper');
+	wp_register_style('main', get_template_directory_uri() . '/css/main.css', array(), false, 'all');
+	wp_enqueue_style('main');
+    wp_register_style('carousel', get_template_directory_uri() . '/css/carousel.css', array(), false, 'all');
+	wp_enqueue_style('carousel');
+    wp_register_style('top-swiper', get_template_directory_uri() . '/css/top-swiper.css', array(), false, 'all');
+	wp_enqueue_style('top-swiper');
+}
+
+add_action('wp_enqueue_scripts', 'load_css');
+
+function load_js()
+{
+	wp_enqueue_script('jquery');
+	wp_register_script('swiper', get_template_directory_uri() . '/js/swiper.min.js', array(), false, true);
+	wp_enqueue_script('swiper');
+	wp_register_script('custom', get_template_directory_uri() . '/js/custom.js', array(), false, true);
+	wp_enqueue_script('custom');
+}
+add_action('wp_enqueue_scripts', 'load_js');
