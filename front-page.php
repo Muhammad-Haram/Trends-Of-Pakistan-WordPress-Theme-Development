2cdcdb59d16dc40d54dc513d093c0bf6c098805e
Muhammad-Haram <haramh643@gmail.com>
2024-09-30 14:17:43 -0700

    header and add some changing

--- front-page.php
+++ front-page.php
@@ -1,1 +1,10 @@
-this is front page
+<?php get_header(); ?>
+
+<div>
+
+    <?php get_template_part('includes/section', 'menu'); ?>
+    <h1><?php the_title();?></h1>
+    
+</div>
+
+<?php get_footer(); ?>
---
+++ includes/section-menu.php
@@ -0,0 +1,68 @@
+<div class="cursor"></div>
+
+<div class="navbar-promax">
+  <!-- header -->
+  <div id="header">
+
+    <div class="navbar-right">
+      <div class="logo">
+        <a href="/">
+             <img class="cursor-color" src="<?= get_template_directory_uri().'/images/logo.png'?>" alt="" />
+        </a>
+      </div>
+
+      <div class="serachDiv cursor-color">
+        <div class="searchLogo">
+          <img class="cursor-color" src="<?= get_template_directory_uri().'/images/search.png'?>" alt="" />
+
+        </div>
+        <input class="searchbar" type="search" placeholder="search" />
+      </div>
+
+    </div>
+
+    <div class="menu-icon bar cursor-color">
+      <img class="menu-icon-img cursor-color" src="<?= get_template_directory_uri().'/images/open-01.png'?>" alt="" />
+
+    </div>
+  </div>
+  <!-- header end -->
+
+  <!-- menu  -->
+  <div class="nav-info">
+
+    <div class="nav-blur"></div>
+    <div class="nav-content">
+      <div class="nav-close">
+
+        <div class="logo">
+          <!-- <img class="cursor-color" src="/assets/img/logo.png" alt=""> -->
+        </div>
+
+        <div class="menu-icon close cursor-color">
+          <img class="cursor-color" src="<?= get_template_directory_uri().'/images/close-01.png'?>" alt="" />
+        </div>
+
+      </div>
+
+      <div class="nav-links">
+        <ul class="nav-links-ul">
+          <li class="nav-links-li"><a class="cursor-color nav-links-anchor" href="">Trending News</a></li>
+          <li class="nav-links-li"><a class="cursor-color nav-links-anchor" href="/sports.html">Sports</a></li>
+          <li class="nav-links-li"><a class="cursor-color nav-links-anchor" href="/health.html">Health</a></li>
+          <li class="nav-links-li"><a class="cursor-color nav-links-anchor" href="/politics.html">politics</a></li>
+          <li class="nav-links-li"><a class="cursor-color nav-links-anchor" href="/tech.html">Tech And Telecom</a>
+          </li>
+          <li class="nav-links-li"><a class="cursor-color nav-links-anchor"
+              href="/entertainments.html">entertainment</a></li>
+          <li class="nav-links-li"><a class="cursor-color nav-links-anchor" href="/education.html">education</a></li>
+          <li class="nav-links-li"><a class="cursor-color nav-links-anchor" href="/business.html">Business</a></li>
+          <li class="nav-links-li"><a class="cursor-color nav-links-anchor" href="/contact.html">Contact Us</a></li>
+
+        </ul>
+      </div>
+    </div>
+
+  </div>
+  <!-- menu end  -->
+</div>
